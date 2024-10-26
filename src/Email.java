a05c42377e1fb1f88fff6aa5a55eb931738d4553
GussRamirez0 <lramirezb10@miumg.edu.gt>
2024-10-26 10:56:40 -0600

    Tarea

---
+++ src/Email.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Email {
+    private List<String> destinatarios;
+    private String asunto;
+    private String cuerpo;
+    private List<String> adjuntos;
+
+    private Email(EmailBuilder builder) {
+        this.destinatarios = builder.destinatarios;
+        this.asunto = builder.asunto;
+        this.cuerpo = builder.cuerpo;
+        this.adjuntos = builder.adjuntos;
+    }
+
+    public void mostrarCorreo() {
+        System.out.println("Destinatarios: " + destinatarios);
+        System.out.println("Asunto: " + asunto);
+        System.out.println("Cuerpo: " + cuerpo);
+        if (adjuntos != null && !adjuntos.isEmpty()) {
+            System.out.println("Adjuntos: " + adjuntos);
+        } else {
+            System.out.println("No hay adjuntos.");
+        }
+    }
+
+    public static class EmailBuilder {
+        private List<String> destinatarios = new ArrayList<>();
+        private String asunto;
+        private String cuerpo;
+        private List<String> adjuntos = new ArrayList<>();
+
+        public EmailBuilder agregarDestinatario(String destinatario) {
+            this.destinatarios.add(destinatario);
+            return this;
+        }
+
+        public EmailBuilder agregarAsunto(String asunto) {
+            this.asunto = asunto;
+            return this;
+        }
+
+        public EmailBuilder agregarCuerpo(String cuerpo) {
+            this.cuerpo = cuerpo;
+            return this;
+        }
+
+        public EmailBuilder agregarAdjunto(String adjunto) {
+            this.adjuntos.add(adjunto);
+            return this;
+        }
+
+        public Email build() {
+            return new Email(this);
+        }
+    }
+
+    public static void main(String[] args) {
+        // Ejemplo de uso del patrón Builder
+        Email email = new Email.EmailBuilder()
+                .agregarDestinatario("lramirezb10@miumg.edu.gt")
+                .agregarDestinatario("br.luisgustavo1@gmail.com")
+                .agregarAsunto("Asunto del Correo")
+                .agregarCuerpo("Cuerpo del Correo")
+                .agregarAdjunto("Archivo2Pdf")
+                .agregarAdjunto("Archivo1png")
+                .build();
+
+        email.mostrarCorreo();
+    }
+}
